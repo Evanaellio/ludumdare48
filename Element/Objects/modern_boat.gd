0cf2e3112570e6f9bbc79b36657a19d8c8f2b728
FoxyHawk <WolfyHawk.Insalan@Gmail.com>
2021-04-24 22:27:18 +0200

    add impressive storytelling boat

---
+++ Element/Objects/modern_boat.gd
@@ -0,0 +1,11 @@
+extends Sprite
+
+
+func _ready():
+	var idle_anim = get_node("AnimationPlayer").get_animation("idle-loop")
+	idle_anim.set_loop(true)
+	get_node("AnimationPlayer").play("idle-loop")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	get_node("AnimationPlayer").play("idle-loop")
