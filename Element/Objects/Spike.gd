8975950e10dd733c5a33847aa5d0e58d804538cf
Tom Spreters <tom.spreters@gmail.com>
2021-04-24 16:20:59 +0200

    Dill + spike prefabs

---
+++ Element/Objects/Spike.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_DamageArea_body_entered(body):
+	print("body entered" + self.get_path_to(body))
+	var p = self.get_path_to(body)
+	var name = p.get_name(p.get_name_count() - 1)
+	
+	if name == "Player" or name.begins_with("Player"):
+		var player = get_node(p)
+		# player.attack()
+		# player.knockback(Vector2(0, -1))
