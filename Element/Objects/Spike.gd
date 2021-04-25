496d0dda6f092d3ca9d0aced4c1c57528a0ba0b7
FoxyHawk <WolfyHawk.Insalan@Gmail.com>
2021-04-25 16:19:24 +0200

    seastar now bumps player

--- Element/Objects/Spike.gd
+++ Element/Objects/Spike.gd
@@ -5,11 +5,11 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_DamageArea_body_entered(body):
-	print("body entered" + self.get_path_to(body))
 	var p = self.get_path_to(body)
 	var name = p.get_name(p.get_name_count() - 1)
 	
 	if name == "Player" or name.begins_with("Player"):
+		print("spike bumper: player bumped")
 		var player = get_node(p)
 		player.hurt()
 		player.knockback(Vector2(0, -25))
