56f28cccf86f1934f2f2d41bab93bfe14d251593
FoxyHawk <WolfyHawk.Insalan@Gmail.com>
2021-04-25 15:37:30 +0200

    correct pufferfish anim

--- Element/Mobs/pufferfish.gd
+++ Element/Mobs/pufferfish.gd
@@ -13,11 +13,14 @@ func _ready():
 
 
 func _on_BumperDetect_body_entered(body):
+	print("pufferfish bumper: body entered" + self.get_path_to(body))
 	var p = self.get_path_to(body)
 	var name = p.get_name(p.get_name_count() - 1)
 	
 	if name == "Player" or name.begins_with("Player"):
+		print("pufferfish bumper: player bumped")
 		var player = get_node(p)
+		player.hurt()
 		player.knockback(Vector2(0, -25))
 
 
@@ -26,15 +29,43 @@ func _on_BumperDetect_body_exited(body):
 
 
 func _on_PlayerDetect_body_entered(body):
-	if body.name == "PlayerBody":
-		anim_idle_position = get_node("AnimationPlayer").current_animation_position
-		get_node("AnimationPlayer").play("growing-once")
+	var p = self.get_path_to(body)
+	var name = p.get_name(p.get_name_count() - 1)
+	
+	if name == "Player" or name.begins_with("Player"):
+		print("pufferfish detect: player entered")
+		
+		self.flip_h = body.global_position.y > self.global_position.y
+		
+		if get_node("AnimationPlayer").current_animation == "idle-loop":
+			anim_idle_position = get_node("AnimationPlayer").current_animation_position
+			get_node("AnimationPlayer").play("growing-once")
+		elif get_node("AnimationPlayer").current_animation == "shrinking-once":
+			print("pufferfish detect: player entered: reverse shrinking")
+			get_node("AnimationPlayer").stop(false)
+			get_node("AnimationPlayer").play_backwards("shrinking-once")
+		else:
+			print("pufferfish detect: player entered: already playing growing")
 
 
 func _on_PlayerDetect_body_exited(body):
-	if body.name == "PlayerBody":
-		anim_idle_position = get_node("AnimationPlayer").current_animation_position
-		get_node("AnimationPlayer").play("shrinking-once")
+	var p = self.get_path_to(body)
+	var name = p.get_name(p.get_name_count() - 1)
+	
+	if name == "Player" or name.begins_with("Player"):
+		print("pufferfish detect: player exited")
+		
+		self.flip_h = body.global_position.y > self.global_position.y
+		
+		if get_node("AnimationPlayer").current_animation == "idle-loop":
+			anim_idle_position = get_node("AnimationPlayer").current_animation_position
+			get_node("AnimationPlayer").play("shrinking-once")
+		elif get_node("AnimationPlayer").current_animation == "growing-once":
+			print("pufferfish detect: player entered: reverse growing")
+			get_node("AnimationPlayer").stop(false)
+			get_node("AnimationPlayer").play_backwards("growing-once")
+		else:
+			print("pufferfish detect: player entered: already playing shrinking")
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
